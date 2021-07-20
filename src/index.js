e5668265ecab323b97bfaf3aa425a96acc6079a5
ALang-c7dg <anne.lang.c7dg@statefarm.com>
2021-07-20 12:58:33 -0500

    added a photo

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './css/main.css';
+// import Image1 from './ronni-kurtz-CB9zVvIRdFE-unsplash.jpg';
+const title = document.getElementById("title");
+title.innerHTML = ("This is my Hello World");
+// add image to section
+// const myImage = new Image();
+// myImage.src = Image1;
+// const element = document.getElementById('mySection');
+// element.appendChild(myImage);
